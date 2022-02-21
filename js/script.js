8fcb311f5dc53ba154f3158c39e21a2dc65e2ea8
benchiz <trigubov.art@mail.ru>
2022-02-21 17:56:24 +0300

    21.02.22

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 const breakpoint = window.matchMedia('(min-width:769px)');
 
 let mySwiper;
+let mySecondSwiper;
 
 const breakpointChecker = function () {
     if (breakpoint.matches === true) {
@@ -17,8 +18,9 @@ const enableSwiper = function () {
     mySwiper = new Swiper('.swiper.course-info__swiper', {
         direction: 'horizontal',
         loop: "true",
+        spaceBetween: 20,
         pagination: {
-            el: '.swiper-pagination.course-info__swiper-pagintation',
+            el: '.swiper-pagination.course-info__swiper-pagination',
         }
     });
 };
@@ -26,10 +28,25 @@ const enableSwiper = function () {
 breakpoint.addListener(breakpointChecker);
 breakpointChecker();
 
-mySwiper = new Swiper('.swiper.program-course__swiper', {
-    direction: 'horizontal',
-    loop: "true",
-    pagination: {
-        el: '.swiper-pagination.program-course__swiper-pagination',
+const enableSecondSwiper = function () {
+    mySecondSwiper = new Swiper('.swiper.program-course__swiper', {
+        direction: 'horizontal',
+        loop: "true",
+        spaceBetween: 20,
+        pagination: {
+            el: '.swiper-pagination.program-course__swiper-pagination',
+        }
+    });
+};
+
+const breakpointSecondChecker = function () {
+    if (breakpoint.matches === true) {
+        if (mySecondSwiper !== undefined) mySecondSwiper.destroy(true, true);
+        return;
+    } else if (breakpoint.matches === false) {
+        return enableSecondSwiper();
     }
-});
+};
+
+breakpoint.addListener(breakpointSecondChecker);
+breakpointSecondChecker();
