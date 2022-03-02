6ffb704ac971b0a87421d0b1b16cb495e38a3e3f
benchiz <trigubov.art@mail.ru>
2022-03-02 18:04:08 +0300

    02.03.22 validation stage

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict"
 
 // $(document).ready(function () {
 const breakpoint = window.matchMedia('(min-width:769px)');
@@ -88,7 +88,9 @@ const breakpointGradChecker = function () {
 breakpointTablet.addListener(breakpointGradChecker);
 breakpointGradChecker();
 
-let btn = document.querySelector('button');
+$('._tel').mask("+7(999) 999-9999");
+let btn = document.querySelector('.js-submit');
+let form = document.querySelector('.js-form');
 
 function validateEmail(mail) {
     const regEx = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
@@ -102,59 +104,85 @@ function validatePhone(phone) {
 
 function checkName() {
     let userName = document.querySelector('._name');
-    if (userName.value < 3) {
-        userName.style.borderColor = 'red';
+    if (userName.value < 2) {
+        userName.classList.add('fail');
+        userName.classList.remove('success');
         return false;
     } else {
-        userName.style.borderColor = 'green';
+        userName.classList.add('success');
+        userName.classList.remove('fail');
         return true;
     }
 }
 
 function checkMail() {
     let mail = document.querySelector('._mail');
-    if (validateEmail(mail)) {
-        mail.style.borderColor = 'green';
+    let mailValue = mail.value;
+    if (validateEmail(mailValue)) {
+        mail.classList.add('success');
+        mail.classList.remove('fail');
         return true;
     } else {
-        mail.style.borderColor = 'red';
+        mail.classList.add('fail');
+        mail.classList.remove('success');
     }
     return false;
 }
 
 function checkPhone() {
     let phone = document.querySelector('._tel');
-    if (validatePhone(phone)) {
-        phone.style.borderColor = 'green';
+    let phoneValue = phone.value;
+    if (validatePhone(phoneValue)) {
+        phone.classList.add('success');
+        phone.classList.remove('fail');
         return true;
     } else {
-        phone.style.borderColor = 'red';
+        phone.classList.add('fail');
+        phone.classList.remove('success');
     }
     return false;
 }
 
 function checkAgreement() {
+    let agreementBlock = document.querySelector('.main_form__confidentiality');
     let agreement = document.querySelector('._agreement');
-    console.log(agreement);
     if (!agreement.checked) {
-        agreement.closest('.main_form__confidentiality').querySelector('label a').style.border = '1px solid red';
+        agreementBlock.classList.add('fail');
         return false;
     } else {
-        agreement.closest('.main_form__confidentiality').querySelector('label a').style.border = '1px solid green';
+        agreementBlock.classList.remove('fail');
         return true;
     }
 }
 
-btn.addEventListener('click', (e) => {
-    e.preventDefault();
-    checkName();
-    checkMail();
-    checkPhone();
-    checkAgreement();
-    if (checkName() && checkMail() && checkPhone() && checkAgreement()) {
-        console.log('Данные отправлены.');
-    } else {
-        console.log('Заполните все данные.');
-    }
-});
-// });
+function validateForm() {
+    btn.addEventListener('click', function (e) {
+        e.preventDefault();
+        checkName();
+        checkMail();
+        checkPhone();
+        checkAgreement();
+        if (checkName() && checkMail() && checkPhone() && checkAgreement()) {
+            let formData = new FormData(form);
+            console.log(formData);
+        // form.addEventListener('submit', function (e) {
+        //     e.preventDefault();
+        //     form.classList.add('sendComplete');
+        //     let response = fetch('sendmail.php', {
+        //         method: 'POST',
+        //         body: formData
+        //     });
+        //     if (response.ok) {
+        //         let result = response.json();
+        //         alert(result.message);
+        //         formPreview.innerHTML = '';
+        //         form.reset();
+        //     } else {
+        //         alert('Ошибка');
+        //     }
+        }
+        console.log('click');
+    });
+}
+
+validateForm();
